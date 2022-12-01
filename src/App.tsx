af1774c10d782883fba43c9ce9ef2f2cf63b3326
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2022-12-01 17:16:04 +0100

    dinamic url

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,25 @@
 import React from "react";
 import "./App.css";
 import { RecipeList } from "./component/recipeList";
-import { SearchMain } from "./component/searchMain";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router";
 import { Recipe } from "./component/recipe";
 import { PageNotFound } from "./component/pageNotFound";
-import { recipes } from "./data/dummyData";
-import { Header } from "./component/header";
+import { SharedLayout } from "./component/sharedLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <SearchMain />
       <Routes>
-        <Route path="/" element={<RecipeList />}></Route>
-        <Route path="recipes">
-          {recipes.map((recipe, index) => (
-            <Route
-              key={`route${index}`} //gdzie jeszcze dać key, żeby nie było warningu w konsoli?
-              path={`/recipes/${recipe.path}`}
-              element={
-                <Recipe
-                  key={`recipe${index}`}
-                  dishName={recipe.fullName}
-                  ingredients={recipe.ingredients}
-                  imageSource={recipe.imageSource}
-                /> //gdzie jeszcze dać key, żeby nie było warningu w konsoli?
-              }
-            />
-          ))}
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<RecipeList />} />
+          <Route path="recipes" />
+          <Route path="recipes/:recipePath" element={<Recipe />} />
         </Route>
-        <Route path="*" element={<PageNotFound />}></Route>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-
 
 export default App;
