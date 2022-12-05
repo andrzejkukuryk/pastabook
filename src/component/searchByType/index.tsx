e90978f7c4abcb68177f438851699d48457dea4d
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2022-12-05 14:35:12 +0100

    add Login, Register

--- src/component/searchByType/index.tsx
+++ src/component/searchByType/index.tsx
@@ -2,14 +2,24 @@ import React from "react";
 import styles from "./style.module.css";
 import { allPastaTypes } from "../../data/dummyData";
 
+interface SearchByTypeProps {
+  typeChecked: boolean[];
+  chooseType: (position: number) => void;
+}
 
-export function SearchByType() {
+export function SearchByType({ typeChecked, chooseType }: SearchByTypeProps) {
   return (
     <div className={styles.container}>
       <form>
         {allPastaTypes.map((type, index) => (
           <label className={styles.listItemLabel} key={`pastaType${index}`}>
-            <input type="checkbox" name="pastaType" value={type} />
+            <input
+              type="checkbox"
+              name="pastaType"
+              value={type}
+              checked={typeChecked[index]}
+              onChange={() => chooseType(index)}
+            />
             {type}
           </label>
         ))}
