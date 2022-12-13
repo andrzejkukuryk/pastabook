aaf212dfb905adfceffedaf7eb803090b0bd3bda
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2022-12-13 16:35:46 +0100

    search works

--- src/component/searchByName/searchByName.tsx
+++ src/component/searchByName/searchByName.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import styles from "./style.module.css";
+import { TYPE_BYNAME } from "../searchMain";
+import { Link, useNavigate } from "react-router-dom";
 
-export function SearchByName() {
+interface SearchByNameProps {
+  searchedPhrase: string;
+  setSearchedPhrase: (phrase: string) => void;
+  searchResult: (searchType: string) => void;
+}
+
+export function SearchByName({
+  searchedPhrase,
+  setSearchedPhrase,
+  searchResult,
+}: SearchByNameProps) {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate("/search");
+    searchResult(TYPE_BYNAME);
+  };
   return (
     <div className={styles.container}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type={"text"}
           className={styles.textInput}
+          value={searchedPhrase}
+          onChange={(e) => setSearchedPhrase(e.target.value)}
           placeholder={"search for a recipe..."}
         ></input>
+
         <input
           type={"submit"}
           className={styles.searchButton}
